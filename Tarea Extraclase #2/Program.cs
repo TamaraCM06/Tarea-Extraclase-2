05d691bf2fe994b2697e0a7671732fe30d3c1cb6
TamaraCM06 <t.cajiao.1@estudiantec.cr>
2024-09-23 11:49:22 -0600

    Revert "Details"
    This reverts commit 05f5eedd664ebbf38aa68c0b84d8b08cf14f23d8.

--- Tarea Extraclase #2/Program.cs
+++ Tarea Extraclase #2/Program.cs
@@ -200,7 +200,6 @@ namespace Tarea2
                 last.Siguiente = nuevoNodo;
                 nuevoNodo.Anterior = last;
                 last = nuevoNodo;
-
             }
         }
 
@@ -259,14 +258,21 @@ namespace Tarea2
                         mergedList.InsertInOrder(nodoB.Dato);
                         nodoB = nodoB.Siguiente;
                     }
+                    while (nodoA != null)
+                    {
+                        mergedList.InsertInOrder(nodoA.Dato);
+                        nodoA = nodoA.Siguiente;
+                    }
+                    while (nodoB != null)
+                    {
+                        mergedList.InsertInOrder(nodoB.Dato);
+                        nodoB = nodoB.Siguiente;
+                    }
 
 
                 }
                 else
                 {
-
-                    nodoA = listAA.last;
-                    nodoB = listBB.last;
 
                     if (nodoA.Dato >= nodoB.Dato)
                     {
@@ -278,35 +284,19 @@ namespace Tarea2
                         mergedList.Insertar(nodoB.Dato);
                         nodoB = nodoB.Anterior;
                     }
+                    while (nodoA != null)
+                    {
+                        mergedList.Insertar(nodoA.Dato);
+                        nodoA = nodoA.Anterior;
+                    }
+                    while (nodoB != null)
+                    {
+                        mergedList.Insertar(nodoB.Dato);
+                        nodoB = nodoB.Anterior;
+                    }
 
                 }
             }
-            while (nodoA != null)
-            {
-                if (direction == SortDirection.Asc)
-                {
-                    mergedList.InsertInOrder(nodoA.Dato);
-                }
-                else
-                {
-                    mergedList.Insertar(nodoA.Dato);
-                }
-                nodoA = nodoA.Siguiente;
-            }
-
-            while (nodoB != null)
-            {
-                if (direction == SortDirection.Asc)
-                {
-                    mergedList.InsertInOrder(nodoB.Dato);
-                }
-                else
-                {
-                    mergedList.Insertar(nodoB.Dato);
-                }
-                nodoB = nodoB.Siguiente;
-            }
-
 
             this.head = mergedList.head;
             this.last = mergedList.last;
